dcc1f4d294954886836cb47f4b0e4516e594ce02
WIN-TE9T2I6AN4G\ICT02_08 <hahataehaha@gmail.com>
2024-09-27 09:44:28 +0900

    md 추가

--- src/test/java/edu/ict/ex/mapper/UserMapperTest.java
+++ src/test/java/edu/ict/ex/mapper/UserMapperTest.java
@@ -1,10 +1,13 @@
 package edu.ict.ex.mapper;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import edu.ict.ex.vo.UserVO;
 import lombok.extern.slf4j.Slf4j;
@@ -16,6 +19,8 @@ class UserMapperTest {
    @Autowired
    private UserMapper userMapper;
    
+   
+   @Disabled
    @Test
    void testUserMapper() {
       assertNotNull(userMapper);
@@ -30,5 +35,40 @@ class UserMapperTest {
       
    }
    
+   
+   
+   @Autowired
+   private PasswordEncoder passwordEncoder;
+   
+   
+   @Disabled
+   @Test
+   void testInsertUser() {
+      
+      UserVO user = new UserVO();
+      user.setUsername("admin2");
+      user.setPassword(passwordEncoder.encode("admin2"));
+      user.setEnabled("1");
+      
+      userMapper.insertUser(user);
+      userMapper.insertAuthorities(user);      
+         
+      assertNotNull(user);
+      
+      System.out.println(user);
+      
+   }
+   
+   @Test
+   void testMather() {
+      
+	   UserVO user = userMapper.getUser("admin2");
+	   //$2a$10$xWAXM9YiSFRlqS8JuBmro.AiVXBNG6Jri.FHarMjBzo42dlDcqlX2
+	   boolean isMatch = passwordEncoder.matches("admin2",user.getPassword());
+	   
+	   assertEquals(isMatch, true);
+
+      
+   }
 
 }
